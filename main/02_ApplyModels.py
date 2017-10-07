abdd83840135981cb19b46160b6cc8aaf7d12800
Jusitn Merz <jrmerz@gmail.com>
2017-10-07 15:26:35 -0700

    simple setup

--- main/02_ApplyModels.py
+++ main/02_ApplyModels.py
@@ -1,5 +1,6 @@
 from pandas import *
 from numpy import *
+from os import path
 
 #################################### NOTES
 """
@@ -10,50 +11,37 @@ This code snippet implements getting predictions using
     2) An array or coefficients
        ...get a mean and uncertainty per pixel/spectrum
 """
-#################################### DEFAULTS
-
-baseDir='D:/Aditya/_AIST/Data/'
-saveDir='D:/Aditya/_AIST/Data/'
-
-# Sample AVIRIS spectra normalized using 01_Normalized.py
-specs=read_csv(baseDir+'ExampleSpectra_Normalized.csv')
-
-# PLSR model coefficients
-# ...one vector (only gets a mean value per pixel)
-coefs_single=read_csv(baseDir+'PLSR_coefficients_Raw_Aggregated_Nitrogen.csv')
-# ...array, gets mean and uncertainty per pixel)
-coefs_multpl=read_csv(baseDir+'PLSR_coefficients_Raw_Full_Nitrogen.csv')
-
-# Output CSVs
-# ...will contain predictions for the single set, as well as for the one with multiple sets that produces uncertainties
-outputCSV=saveDir+'ExampleOutput_Predictions.csv'
 
 #################################### PROCESS
 
-# Get coefficients
-# ...single set
-iCoef=array(coefs_single['mean'])
-# ...array
-mCoef=array(coefs_multpl)
+def run(spectra, coefficients_dir):
+    # PLSR model coefficients
+    # ...one vector (only gets a mean value per pixel)
+    coefs_single=read_csv(path.join(coefficients_dir, 'PLSR_coefficients_Raw_Aggregated_Nitrogen.csv'))
+    # ...array, gets mean and uncertainty per pixel)
+    coefs_multpl=read_csv(path.join(coefficients_dir, 'PLSR_coefficients_Raw_Full_Nitrogen.csv'))
 
-# Get matrix of spectra
-iSpec=array(specs)
+    # Get coefficients
+    # ...single set
+    iCoef=array(coefs_single['mean'])
+    # ...array
+    mCoef=array(coefs_multpl)
 
-# Add vector of ones to accomodate intercept
-iSpec=hstack((ones((iSpec.shape[0],1)),iSpec))
+    # Get matrix of spectra
+    iSpec=array(spectra)
 
-# Matrix multiply iCoef and iSpec to get single vector of predictions
-iPred=iSpec.dot(iCoef)
+    # Add vector of ones to accomodate intercept
+    iSpec=hstack((ones((iSpec.shape[0],1)),iSpec))
 
-# Matrix multiply mCoef and iSpec to get multiple predictions
-mPred=iSpec.dot(mCoef.T)
-# ...aggregate predictions to get means and uncertainties
-avePred=mPred.mean(axis=1)
-stdPred=mPred.std(axis=1)
+    # Matrix multiply iCoef and iSpec to get single vector of predictions
+    iPred=iSpec.dot(iCoef)
 
-####################### Compile all into once CSV for comparison
-outPreds=vstack((iPred,avePred,stdPred)).T
-outDF=DataFrame(columns=['NIT_Single','NIT_MultMean','NIT_MultStdv'],data=outPreds)
-outDF.to_csv(outputCSV,index=False)
-print '...Done'
+    # Matrix multiply mCoef and iSpec to get multiple predictions
+    mPred=iSpec.dot(mCoef.T)
+    # ...aggregate predictions to get means and uncertainties
+    avePred=mPred.mean(axis=1)
+    stdPred=mPred.std(axis=1)
 
+    ####################### Compile all into once CSV for comparison
+    outPreds=vstack((iPred,avePred,stdPred)).T
+    return DataFrame(columns=['NIT_Single','NIT_MultMean','NIT_MultStdv'],data=outPreds)
--- transform/01_Normalize.py
+++ transform/01_Normalize.py
@@ -11,36 +11,27 @@ Process:
     4) the original spectrum divided by the result of 3)
     5) spectra written to an output CSV
 """
-#################################### DEFAULTS
-
-baseDir='D:/Aditya/_AIST/Data/'
-saveDir='D:/Aditya/_AIST/Data/'
-
-
-
-# Output filename for vector normalized spectra
-outputName=saveDir+'ExampleSpectra_Normalized.csv'
 
 #################################### PROCESS
 
 def transform(spectra, args):
 
     # Sample raw AVIRIS spectra
-    specs=read_csv(baseDir+'ExampleSpectra.csv')
+    # specs=read_csv(baseDir+'ExampleSpectra.csv')
+
     # CSV containing bad bands set to 1s
-    bands=read_csv(baseDir+'Bands.csv')
+    bands=read_csv(args.bands)
 
     # Get bad band list (bad bands: IsBad==1, good bands: IsBad==0)
     badBands=bands['IsBad'].values
 
     # Initialize array to hold normalized spectra
-    outMat=zeros(specs.shape)
+    outMat=zeros(spectra.shape)
 
     # ...for each spectrum:
-    for i in specs.index:
-        print i
+    for i in spectra.index:
         # Extract row
-        iSpec=array(specs.ix[i][:])
+        iSpec=array(spectra.ix[i][:])
         # Set bad bands to zeros
         iSpec[badBands==1]=0
         # Find sqrt of sum of squares of all values
@@ -50,7 +41,5 @@ def transform(spectra, args):
         # Store results
         outMat[i,:]=nSpec
 
-    # Save normalized spectra to CSV
-    outDF=DataFrame(columns=specs.columns, data=outMat)
-    outDF.to_csv(outputName,index=False)
-    print '...Done'
+    # Return normalized spectra dataframe to EcoSML library
+    return DataFrame(columns=spectra.columns, data=outMat)
